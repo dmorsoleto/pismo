7cc68e3edb3ed67baeda7b91a8fa0a81daa0e395
Daniel Morsoleto <dmorsoleto@gmail.com>
2023-11-08 18:13:36 -0300

    feat: mock for accounts usecase

---
+++ internal/tests/mocks/usecase/accounts/account.go
@@ -0,0 +1,22 @@
+package accountsmocks
+
+import (
+	"dmorsoleto/internal/entity"
+	"dmorsoleto/internal/gateways/repository/accounts"
+
+	"github.com/stretchr/testify/mock"
+)
+
+type AccountsUseCaseMock struct {
+	mock.Mock
+}
+
+func (m *AccountsUseCaseMock) Get(id string) (entity.Account, error) {
+	args := m.Called(id)
+	return args.Get(0).(entity.Account), args.Error(1)
+}
+
+func (m *AccountsUseCaseMock) Add(account accounts.AddAccount) error {
+	args := m.Called(account)
+	return args.Error(0)
+}
