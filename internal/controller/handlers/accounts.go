da95df4c9f4f5e6321ed208ab6c445efaae537a5
Daniel Morsoleto <dmorsoleto@gmail.com>
2023-11-08 18:54:43 -0300

    refactor: accounts controller

---
+++ internal/controller/handlers/accounts.go
@@ -0,0 +1,65 @@
+package handlers
+
+import (
+	"dmorsoleto/internal/usecase/accounts"
+	"encoding/json"
+	"net/http"
+
+	accounts_repo "dmorsoleto/internal/gateways/repository/accounts"
+)
+
+type AccountsHandler interface {
+	GetAccount(w http.ResponseWriter, r *http.Request)
+	AddAccount(w http.ResponseWriter, r *http.Request)
+}
+
+type accountsHandler struct {
+	accountsUseCase accounts.AccountsUseCase
+}
+
+func NewAccountsHandler(accountsUseCase accounts.AccountsUseCase) AccountsHandler {
+	return &accountsHandler{
+		accountsUseCase: accountsUseCase,
+	}
+}
+
+type ResponseData struct {
+	Message string `json:"message"`
+}
+
+func (ref *accountsHandler) GetAccount(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("accountId")
+
+	account, err := ref.accountsUseCase.Get(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	jsonBytes, err := json.Marshal(account)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonBytes)
+}
+
+func (ref *accountsHandler) AddAccount(w http.ResponseWriter, r *http.Request) {
+	var account accounts_repo.AddAccount
+
+	err := json.NewDecoder(r.Body).Decode(&account)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = ref.accountsUseCase.Add(account)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
---
+++ internal/controller/handlers/accounts_test.go
@@ -0,0 +1,170 @@
+package handlers
+
+import (
+	"bytes"
+	"dmorsoleto/internal/entity"
+	"dmorsoleto/internal/gateways/repository/accounts"
+	accountsmocks "dmorsoleto/internal/tests/mocks/usecase/accounts"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+var (
+	errFoo = errors.New("foo")
+)
+
+type AccountsHandlerTestSuite struct {
+	suite.Suite
+	accountsHandler AccountsHandler
+	accountsUseCase *accountsmocks.AccountsUseCaseMock
+}
+
+func (ref *AccountsHandlerTestSuite) SetupTest() {
+	ref.accountsUseCase = new(accountsmocks.AccountsUseCaseMock)
+	ref.accountsHandler = NewAccountsHandler(ref.accountsUseCase)
+}
+
+func TestAccountsHandlerTestSuite(t *testing.T) {
+	suiteTest := &AccountsHandlerTestSuite{}
+	suite.Run(t, suiteTest)
+}
+
+func (ref *AccountsHandlerTestSuite) TestGetAccount_Sucess() {
+	req, err := http.NewRequest("GET", "/account?accountId=1", nil)
+	if err != nil {
+		ref.T().Fatal(err)
+	}
+
+	w := httptest.NewRecorder()
+
+	expectedAccount := newAccountEntity()
+
+	ref.accountsUseCase.On("Get", "1").Return(expectedAccount, nil)
+
+	ref.accountsHandler.GetAccount(w, req)
+
+	if w.Code != http.StatusOK {
+		ref.T().Errorf("Expected status code %d, got %d", http.StatusOK, w.Code)
+	}
+
+	expected := `{"account_id":"cb1fd9fa-6d15-4a5d-8543-ae1ac1fbd563","document_number":"123456789"}`
+	if w.Body.String() != expected {
+		ref.T().Errorf("Expected body %s, got %s", expected, w.Body.String())
+	}
+
+	ref.accountsUseCase.AssertExpectations(ref.T())
+}
+
+func (ref *AccountsHandlerTestSuite) TestGetAccount_UseCase_Error() {
+	req, err := http.NewRequest("GET", "/account?accountId=1", nil)
+	if err != nil {
+		ref.T().Fatal(err)
+	}
+
+	w := httptest.NewRecorder()
+
+	ref.accountsUseCase.On("Get", "1").Return(entity.Account{}, errFoo)
+
+	ref.accountsHandler.GetAccount(w, req)
+
+	if w.Code != http.StatusInternalServerError {
+		ref.T().Errorf("Expected status code %d, got %d", http.StatusOK, w.Code)
+	}
+
+	ref.accountsUseCase.AssertExpectations(ref.T())
+}
+
+func (ref *AccountsHandlerTestSuite) TestAddAccount_Success() {
+	newAccount := newAddAccount()
+
+	payloadAccount, err := json.Marshal(newAccount)
+	if err != nil {
+		ref.T().Fatal(err)
+	}
+
+	req, err := http.NewRequest("POST", "/account", bytes.NewBuffer(payloadAccount))
+	if err != nil {
+		ref.T().Fatal(err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	w := httptest.NewRecorder()
+
+	ref.accountsUseCase.On("Add", newAccount).Return(nil)
+
+	ref.accountsHandler.AddAccount(w, req)
+
+	if w.Code != http.StatusCreated {
+		ref.T().Errorf("Expected status code %d, got %d", http.StatusOK, w.Code)
+	}
+
+	ref.accountsUseCase.AssertExpectations(ref.T())
+}
+
+func (ref *AccountsHandlerTestSuite) TestAddAccount_UseCase_Error() {
+	newAccount := newAddAccount()
+
+	payloadAccount, err := json.Marshal(newAccount)
+	if err != nil {
+		ref.T().Fatal(err)
+	}
+
+	req, err := http.NewRequest("POST", "/account", bytes.NewBuffer(payloadAccount))
+	if err != nil {
+		ref.T().Fatal(err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	w := httptest.NewRecorder()
+
+	ref.accountsUseCase.On("Add", newAccount).Return(errFoo)
+
+	ref.accountsHandler.AddAccount(w, req)
+
+	if w.Code != http.StatusInternalServerError {
+		ref.T().Errorf("Expected status code %d, got %d", http.StatusOK, w.Code)
+	}
+
+	ref.accountsUseCase.AssertExpectations(ref.T())
+}
+
+func (ref *AccountsHandlerTestSuite) TestAddAccount_Request_Error() {
+	teste := []byte{1, 2, 3}
+
+	req, err := http.NewRequest("POST", "/account", bytes.NewBuffer(teste))
+	if err != nil {
+		ref.T().Fatal(err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	w := httptest.NewRecorder()
+
+	ref.accountsHandler.AddAccount(w, req)
+
+	if w.Code != http.StatusBadRequest {
+		ref.T().Errorf("Expected status code %d, got %d", http.StatusOK, w.Code)
+	}
+
+	ref.accountsUseCase.AssertExpectations(ref.T())
+}
+
+func newAccountEntity() entity.Account {
+	return entity.Account{
+		AccountID:      "cb1fd9fa-6d15-4a5d-8543-ae1ac1fbd563",
+		DocumentNumber: "123456789",
+	}
+}
+
+func newAddAccount() accounts.AddAccount {
+	return accounts.AddAccount{
+		DocumentNumber: "123456789",
+	}
+}
